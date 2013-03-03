c0e380f00d94ca07796c838875ab75c855037ad8
Mike Kirkup <mkirkup@terepac.com>
2013-03-03 16:42:16 -0500

    Added logger

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,19 @@
 package controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
+
+    /**
+     * Defines the logger that will be used for logging across the system.
+     */
+    private static Logger _logger = LoggerFactory.getLogger("controllers.SignUp");    
   
     public static Result index() {
         return ok(index.render());
